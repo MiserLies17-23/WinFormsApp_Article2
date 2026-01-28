90a4b1216f523468de4ca80b818ad1b060cfed67
MiesLies17-23 <vadimtolstouhov662@gmail.com>
2026-01-28 11:34:24 +0300

    Добавил новые компоненты для построения правильной архитектуры

---
+++ WinFormsApp_Article/BenchMark/MethodResults.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// BanchMark-компонент для подсчёт результатов 
+/// </summary>
+public class MethodResults
+{
+    public string? AlgorithmName { get; set; }
+    public int InsertTime { get; set; }
+    public int SearchTime { get; set; }
+    public int Сomparisons { get; set; }
+
+}
+
---
+++ WinFormsApp_Article/Service/DataService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+/// <summary>
+/// Сервис - основной слой бизнес-логики
+/// </summary>
+public class DataService
+{
+    private List<MethodResults> getAllResults(int[] array, Func<int,int,int> method)
+    {
+        List<MethodResults> results = [];
+
+        MethodResults linerProbingResults = getLinerProbingHenerate(array, method);
+        results.Add(linerProbingResults);
+        return results;
+    }
+
+    private MethodResults getLinerProbingHenerate(int[] array, Func<int, int, int> method)
+    {
+        (int[] moa, int insertTime) = CollisionAlgorithms.LinerProbingInsert(array, 
+            method);
+        (int sum, int founds, int searchTime) = CollisionAlgorithms.LinerProbingSearch(moa,
+            method);
+        return new MethodResults
+        {
+            AlgorithmName = "Линейное пробирование",
+            InsertTime = insertTime,
+            SearchTime = searchTime,
+            Сomparisons = sum,
+        };
+    }
+
+    private int[] ArrayGenerate(int size)
+    {
+        Random random = new();
+        int[] array = new int[size];
+        for (int i = 0; i < array.Length; i++) 
+            array[i] = random.Next();
+        return array;
+    }
+
+    public List<MethodResults> Run(int size, Func<int,int,int> method)
+    {
+        int[] array = ArrayGenerate(size);
+        var results = getAllResults(array, method);
+        return results;
+    }
+}
