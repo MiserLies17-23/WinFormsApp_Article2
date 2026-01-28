464cb6d0a26c3b10affc75253eadbcdea4aecb9b
MiesLies17-23 <vadimtolstouhov662@gmail.com>
2026-01-28 11:35:01 +0300

    Сделал пробный работающий вариант

--- WinFormsApp_Article/Algorithms/CollisionAlgorithms.cs
+++ WinFormsApp_Article/Algorithms/CollisionAlgorithms.cs
@@ -1,57 +1,82 @@
 ﻿using System;
-using System.CodeDom;
-using System.Drawing;
-
-
-namespace WinFormsApp_Article.Algorithms
+public static class CollisionAlgorithms
 {
-    public static class CollisionAlgorithms
+    public static (int[], int) LinerProbingInsert(int[] array, Func<int, int, int> method)
     {
-        public static int[] LinerProbingInsert(int[] array, Func<int, int, int> method,
-            int div)
+        int[] moa = new int[array.Length];
+        Array.Fill(moa, -1);
+        int startTime = Environment.TickCount;
+        foreach (int num in array)
         {
-            int[] moa = new int[array.Length];
-            Array.Fill(moa, -1);
-            foreach (int num in array)
+            int hash = method((int)num, array.Length);
+            int item = 0;
+            while (moa[hash % moa.Length] != -1 && item < moa.Length)
             {
-                int hash = method((int)num, div);
-                int item = 0;
-                while (moa[hash % moa.Length] != -1 && item < moa.Length)
-                {
-                    hash++;
-                    item++;
-                }
-                if (item == moa.Length)
-                    throw new Exception("Массив зареполнен!");
-                moa[hash % moa.Length] = num;
+                hash++;
+                item++;
             }
-            return moa;
+            if (item == moa.Length)
+                throw new Exception("Массив заполнен!");
+            moa[hash % moa.Length] = num;
         }
+        int endTime = Environment.TickCount - startTime;
+        return (moa, endTime);
+    }
 
-        public static (int, int, int) LinerProbingSearch(int[] moa, Func<int, int, int> method)
+    public static (int, int, int) LinerProbingSearch(int[] moa, 
+        Func<int, int, int> method)
+    {
+        int sum = 0, founds = 0, endTime;
+
+        int startTime = Environment.TickCount;
+        foreach (int num in moa)
         {
-            int sum = 0, founds = 0, endTime;
+            int hash = method(num, moa.Length);
 
-            int startTime = Environment.TickCount;
-            foreach (int num in moa)
+            for (int i = 0; i < moa.Length; i++)
             {
-                int hash = method(num, moa.Length);
-
-                for (int i = 0; i < moa.Length; i++)
-                {
-                    sum++;
-                    if (num == moa[hash % moa.Length])
-                    {
-                        founds++;
-                        break;
-                    }
-                    hash++;
-                }
                 sum++;
+                if (num == moa[hash % moa.Length])
+                {
+                    founds++;
+                    break;
+                }
+                hash++;
             }
-            endTime = Environment.TickCount - startTime;
-            
-            return (sum, founds, endTime);
+            sum++;
         }
+        endTime = Environment.TickCount - startTime;
+            
+        return (sum, founds, endTime);
+    }
+
+    public static int[] QuadraticProbing(int[] array,
+        Func<int, int, int> method)
+    {
+        int[] moa = new int[array.Length];
+        Array.Fill(moa, -1);
+        foreach (int num in array)
+        {
+            int hash = method((int)num, array.Length);
+            int item = 0;
+            int c = 1;
+            while (moa[hash % moa.Length] != -1 && item < moa.Length)
+            {
+                hash += c * c;
+                c++;
+                item++;
+            }
+            if (item == moa.Length)
+                throw new Exception("Массив заполнен!");
+            moa[hash % moa.Length] = num;
+        }
+        return [];
+    }
+
+    public static int[] DoubleHashing(int[] array,
+        Func<int, int, int> method1,
+        Func<int, int, int> method2)
+    {
+        return [];
     }
 }
--- WinFormsApp_Article/Article_Form.cs
+++ WinFormsApp_Article/Article_Form.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using System.Windows.Forms;
 
 namespace WinFormsApp_Article
 {
+    /// <summary>
+    /// UI-компонент для взаимодействия с пользователем
+    /// </summary>
     public partial class Article_Form : Form
     {
-        
 
         public Article_Form()
         {
             InitializeComponent();
-            
         }
 
         private void Article_Form_Load(object sender, EventArgs e)
@@ -28,7 +30,46 @@ namespace WinFormsApp_Article
 
         private void CompareButton_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                Func<int, int, int> currentMethod = getCurrentMethod();
+                int size = (int)SizeNumericUpDown.Value;
+                
+                DataService service = new DataService();
+                var results = service.Run(size, currentMethod);
+                DisplayData(results);
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private Func<int, int, int> getCurrentMethod()
+        {
+            string methodName = HashComboBox.Text;
+            return methodName switch
+            {
+                "Метод деления" => (Func<int, int, int>)Algorithms.HashAlgorithms.DivisionMethod,
+                "Метод середины квадрата" => (Func<int, int, int>)Algorithms.HashAlgorithms.MidsquareMethod,
+                "Метод умножения" => (Func<int, int, int>)Algorithms.HashAlgorithms.MultiplicationMethod,
+                "Метод свёртывания" => (Func<int, int, int>)Algorithms.HashAlgorithms.FoldingMethod,
+                _ => throw new ArgumentException("Метод не распознан!"),
+            };
+        }
+
+        private void DisplayData(List<MethodResults> results)
+        {
+            dataGridView1.Rows.Clear();
+            foreach (var result in results)
+            {
+                dataGridView1.Rows.Add(
+                    result.AlgorithmName,
+                    result.InsertTime,
+                    result.SearchTime,
+                    result.Сomparisons
+                );
+            }
         }
     }
 }
